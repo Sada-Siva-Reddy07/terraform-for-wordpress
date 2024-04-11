5c934c2b82c1b7692e11661ecf90c9ceb0db2959
SADA SIVA REDDY <145244419+Sada-Siva-Reddy07@users.noreply.github.com>
2024-04-11 23:10:04 +0530

    Update EC2.tf

--- EC2.tf
+++ EC2.tf
@@ -1,6 +1,6 @@
 #instance
 resource "aws_instance" "wordpress8" {
-  ami                         = "ami-04175dfed7619fb38"
+  ami                         = "ami-0f0ec0d37d04440e3"
   instance_type               = "t3.micro"
   key_name                    = "stockhol"
   vpc_security_group_ids      = [aws_security_group.web-sg.id]
